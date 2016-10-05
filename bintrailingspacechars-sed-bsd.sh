7d4300be7b30d1c34aa16435d479f647139dd12d
Douglas Mencken <dougmencken@gmail.com>
2016-10-05 08:43:07 -0400

    add bintrailingspacechars-sed-bsd.sh script

---
+++ bintrailingspacechars-sed-bsd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+find . -iname '*.*pp' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
+find . -iname '*.c' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
+find . -iname '*.txt' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
