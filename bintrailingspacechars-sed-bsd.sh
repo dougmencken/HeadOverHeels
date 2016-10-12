a34d21e95966416ffdba59fe9b2997928fc4ccca
Douglas Mencken <dougmencken@gmail.com>
2016-10-11 23:47:07 -0400

    bin trailing space in eXtensible Markup Language files too

--- bintrailingspacechars-sed-bsd.sh
+++ bintrailingspacechars-sed-bsd.sh
@@ -3,3 +3,4 @@
 find . -iname '*.*pp' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
 find . -iname '*.c' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
 find . -iname '*.txt' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
+find . -iname '*.xml' -type f -exec sed -i '' 's/[[:space:]]\{1,\}$//' {} \+
