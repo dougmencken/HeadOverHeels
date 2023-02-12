e4c863900373e1d142b15ea4f41902df058407a5
Douglas Mencken <dougmencken@gmail.com>
2023-02-12 08:42:57 +0300

    fix another configure error
    (c) 2023

--- src/TimerPOSIX.hpp
+++ src/TimerPOSIX.hpp
@@ -1,6 +1,6 @@
 // The free and open source remake of Head over Heels
 //
-// Copyright © 2022 Douglas Mencken dougmencken@gmail.com
+// Copyright © 2023 Douglas Mencken dougmencken@gmail.com
 // Copyright © 2008 Jorge Rodríguez Santos
 // Original game copyright © 1987 Ocean Software Ltd.
 //
